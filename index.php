f769506628c9f62da64b8f0aaed88f33790c6cfa
Terry Bu <ktbucifer@gmail.com>
2014-06-26 18:31:07 -0400

    finally got scrolldown effect jquery to work

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--LOAD BOOTSTRAP -->
-
+  <!--LOAD jQuery -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="_javascript/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+  <script src="_javascript/vendor/bootstrap.min.js"></script>
   <!--LOAD YOUR STYLESHEETS -->
   <link href="_css/application.css" rel="stylesheet" type="text/css">
   <title>Terry Bu - Portfolio Website | Home</title>
-  <!--[if lt IE 9]>
-  <script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
-  <![endif]-->
+  <!--[if lt IE 9]><script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script><![endif]-->
 </head>
 <body>
-  <!--[if lt IE 7]>
-  <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-  <![endif]-->
+  <!--[if lt IE 7]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
   <div class="container">
     <div id="browser"><p>*Optimal in 1920x1080 resolution/Chrome</p></div>
     <?php include("_includes/gotSocial.php"); ?>
@@ -27,7 +24,6 @@
 
     <div id="bodyContent">
       <p>Click or Hover. Click again to close</p>
-      
 
       <img src="_images/arrows.jpg" id="arrow">
       <div id="leftharsh">
@@ -85,21 +81,11 @@
 
 
 <!--BELOW is javascript-->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="_javascript/vendor/jquery-1.11.0.min.js"><\/script>')</script>
-<script src="_javascript/vendor/bootstrap.min.js"></script>
+
 
 <script type="text/javascript" src="_javascript/sound-mouseover.js">
 </script>
-<!-->
-<script>
-$('.dropdown-toggle').click(
-  function() {
-    $( '#bodyContent' ).slideToggle();
-  }
-);
-</script>
--->
+
 <audio>
 <source src="_sound/click.mp3"></source>
 <source src="_sound/click.ogg"></source>
